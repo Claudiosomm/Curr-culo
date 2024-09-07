98719e79503c35ae7a0bd5118f282591d4fe1066
Claudio Silva <156344616+Claudiosomm@users.noreply.github.com>
2024-09-07 05:55:02 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,34 @@
+let dados = [
+    {
+        titulo : "Linkedin",
+        descricao : "Cláudio é um camarada que com seus 13 anos começou a trabalhar por necessidade em uma lanchonete, e por esse motivo não teve tempo de estudar. Na oitava série parou de estudar, já que nessa época trabalhava em uma padaria de domingo a domingo e como todos sabem, nesse ramo de atividade trabalha-se muito mesmo. Aos vinte anos entrou em um restaurante, e logo de cara foi promovido a ajudante de Sommelier, pois segundo seus chefes ele era rápido, e dessa forma seria bom para o setor. Após dois anos Atuando como ajudante, passou a executar a função como profissional. Depois de sete anos mudou de empresa, trocando a função para exercer como Maitre, onde trabalhou por quase dez anos. Os tempos passaram, e aos quarenta anos resolveu estudar para trabalhar em um setor de sua escolha, e foi aí que escolheu trabalhar na área de tecnologia. hoje está no terceiro semestre cursando gestão de tecnologia na Instituição Eniac. Essa migração não tem sido fácil por conta de sua idade, é o que acredita, mas com o network da imersão do Gemini pode ser que tudo mude, pois nele tem muita gente importante que certamente está precisando de algum funcionário iniciante para moldar do jeito deles.",
+        link : "http://www.linkedin.com/in/claudiosilvatecnologia",
+        tags: "claudio Claudio Cláudio cláudio LINKEDIN linkedin"
+        
+    },
+    {
+        titulo : "Facebook",
+        descricao : "Cláudio é um homem familia, tendo como dependentes sua esposa e dois filhos, que com tanto carinho e atenção se torna presente mesmo com as correrias atuais, dividindo a atenção com o trabalho e estudo. Ele gosta de tocar violão,jogar futebol, resolver cubo mágico, e é claro, trabalhar é o forte dele. Veja um pouco sobre ele em seu facebook.",
+        link : "https://www.facebook.com/claudiosomm",
+        tags: "face Face FACE"
+    },
+    {
+        titulo : "Tik Tok",
+        descricao : "No tik tok o Cláudio é bem brincalhão. Ele não gosta de ficar postando todos os dias, porém sempre que acontece algo diferente, para ser visto, acaba postando aalgumas coisas.",
+        link : "https://www.tiktok.com/@claudiosilva9807?_t=8pTGjUu2TJ8&_r=1",
+        tags: "tik tok Tik Tok TIK TOK"
+    },
+    {
+        titulo : "Instagran",
+        descricao : "No instagran ele coloca apenas ítens que lhe trazem boas lembranças. Normalmente ele não posta muito, mas você consegue ver um pouco da tragetória dele na vida pessoal e profissional.",
+        link : "https://www.instagram.com/claudiosomm/?igsh=MWxmaGZ0YWZtMTlyeg%3D%3D",
+        tags: "insta Insta INSTAGRAN"
+    }
+    ];
+    
+    
+
+
+
+
+    
